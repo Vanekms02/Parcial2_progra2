da9388ed854d306a5cd46a057707b95d5bc86fcb
Vane <kmoraless15@gmail.edu.gt>
2026-09-24 19:12:58 -0600

    fix rutas tareas

--- app/routes/tarea.route.js
+++ app/routes/tarea.route.js
@@ -2,10 +2,10 @@ module.exports = app => {
     const tarea = require("../controllers/tarea.controller.js");
     var router = require("express").Router();
 
-    router.post("/create", tarea.create);
-    router.put("/update/:id", tarea.update); 
-    router.get("/getAll", tarea.findAll);
-    router.delete("/delete/:id", tarea.delete);
+    router.get("/", tarea.findAll);
+    router.post("/", tarea.create);
+    router.put("/:id", tarea.update);
+    router.delete("/:id", tarea.delete);
 
-    app.use("/api/homework", router);
+    app.use("/tareas", router);
 };
